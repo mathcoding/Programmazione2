cf269064fc5cef1f1e3f305895497a3b664d48ab
Stefano Gualandi <stefano.gualandi@gmail.com>
2017-03-23 11:37:50 +0100

    Attenzione all'identazione

--- Assignments/hw1/compito_2.py
+++ Assignments/hw1/compito_2.py
@@ -5,46 +5,47 @@ def NomeCognome():
 ##-------------------------------
 
 def And(Ls):
-	# DA COMPLETARE
-	pass
+    # DA COMPLETARE
+    pass
 
 def Or(Ls):
-	# DA COMPLETARE
-	pass
+    # DA COMPLETARE
+    pass
 
 def Length(Ls):
-	# DA COMPLETARE
-	pass
+    # DA COMPLETARE
+    pass
 
 def Reverse(Ls):
-	# DA COMPLETARE
-	pass
+    # DA COMPLETARE
+    pass
 
 def FoldFactorial(Ls):
-	# DA COMPLETARE
-	pass
+    # DA COMPLETARE
+    pass
 
 def SumLength(Ls):
-	# DA COMPLETARE
-	pass
+    # DA COMPLETARE
+    pass
 
 def Map(Ls):
-	# DA COMPLETARE
-	pass
+    # DA COMPLETARE
+    pass
 
 def Filter(Ls):
-	# DA COMPLETARE
-	pass
+    # DA COMPLETARE
+    pass
 
 def FoldLeft(Ls):
-	# DA COMPLETARE
-	pass
+    # DA COMPLETARE
+    pass
 
 ## PARTE SECONDA: LISTA INFINITA NUMERI PRIMI
 ##-------------------------------------------
+
 def NumeriPrimi():
-	# DA COMPLETARE
-	pass
+    # DA COMPLETARE
+    pass
 
 def Fibonacci():
     # DA COMPLETARE (FACOLTATIVO)
@@ -52,6 +53,7 @@ def Fibonacci():
 
 ## PARTE TERZA: JULIA SET E FRATTALI
 ##----------------------------------
+
 def JuliaSetRec(z, c, k, max_k=64):
     # DA COMPLETARE
     pass
